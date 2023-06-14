1b75955a5f4a0faac8d3963dc7ccfa8ad9c5c96b
Kun Li <kun@moderne.io>
2023-06-14 10:47:32 -0700

    Bump Jackson to the latest version 2.15.2 to avoid dependency vulnerbility report

--- src/main/java/org/openrewrite/gradle/RewriteJavaPlugin.java
+++ src/main/java/org/openrewrite/gradle/RewriteJavaPlugin.java
@@ -37,7 +37,7 @@ public class RewriteJavaPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         RewriteJavaExtension ext = project.getExtensions().create("rewriteJava", RewriteJavaExtension.class);
-        ext.getJacksonVersion().convention("2.15.0");
+        ext.getJacksonVersion().convention("2.15.2");
 
         project.getPlugins().apply(JavaLibraryPlugin.class);
         project.getPlugins().apply(RewriteDependencyRepositoriesPlugin.class);
