2a640b72cc9cf210dabd0b40ef5c0a5d4834c6bc
Tim te Beek <tim@moderne.io>
2024-01-09 14:07:12 +0100

    Pin Nebula release plugin over "Git user.name is not set." (#28)
    * Pin Nebula release plugin over "Git user.name is not set."
    
    * Update build.gradle.kts
    
    Co-authored-by: Peter Streef <p.streef@gmail.com>
    
    * Update build.gradle.kts
    
    * Update build.gradle.kts
    
    ---------
    
    Co-authored-by: Peter Streef <p.streef@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -165,7 +165,9 @@ dependencies {
     implementation("org.owasp:dependency-check-gradle:latest.release")
     implementation("com.netflix.nebula:gradle-contacts-plugin:latest.release")
     implementation("com.netflix.nebula:gradle-info-plugin:latest.release")
-    implementation("com.netflix.nebula:nebula-release-plugin:latest.release")
+    implementation("com.netflix.nebula:nebula-release-plugin:18.0.+") {
+        because("Git user.name is not set.");
+    }
     implementation("com.netflix.nebula:nebula-publishing-plugin:latest.release")
     implementation("com.netflix.nebula:nebula-project-plugin:latest.release")
     implementation("io.github.gradle-nexus:publish-plugin:latest.release")
