0e8ed6a76bc3df5e992201fa58a2b8eb8f05fdb5
Tim te Beek <tim@moderne.io>
2025-07-28 12:25:27 +0200

    Organize imports

--- src/main/java/org/openrewrite/gradle/RewriteRecipeAuthorAttributionPlugin.java
+++ src/main/java/org/openrewrite/gradle/RewriteRecipeAuthorAttributionPlugin.java
@@ -17,8 +17,6 @@ package org.openrewrite.gradle;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.file.DuplicatesStrategy;
-import org.gradle.api.file.FileCollection;
 import org.gradle.api.plugins.JavaPluginExtension;
 import org.gradle.api.tasks.Copy;
 import org.gradle.api.tasks.SourceSet;
@@ -27,12 +25,7 @@ import org.gradle.api.tasks.TaskProvider;
 import org.gradle.language.jvm.tasks.ProcessResources;
 
 import java.io.File;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
-import static java.util.stream.Collectors.toSet;
 import static org.eclipse.jgit.util.StringUtils.capitalize;
 
 public class RewriteRecipeAuthorAttributionPlugin implements Plugin<Project> {
