a8a014f996b3249a33a63f309821f75a6bbbc7b6
Tim te Beek <timtebeek@gmail.com>
2025-11-10 10:25:52 +0000

    OpenRewrite recipe best practices
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.recipes.rewrite.OpenRewriteRecipeBestPractices?organizationId=QUxML09wZW4gU291cmNlL09wZW5SZXdyaXRl
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/main/java/org/openrewrite/gradle/RewriteRecipeMarketplacePlugin.java
+++ src/main/java/org/openrewrite/gradle/RewriteRecipeMarketplacePlugin.java
@@ -44,8 +44,7 @@ public class RewriteRecipeMarketplacePlugin implements Plugin<Project> {
         // Configure check task when Java plugin is applied
         project.getPlugins().withType(JavaPlugin.class, javaPlugin ->
             // Add CSV validation to the check phase
-            project.getTasks().named("check").configure(check -> {
-                check.dependsOn("recipeCsvValidate");
-            }));
+            project.getTasks().named("check").configure(check ->
+                check.dependsOn("recipeCsvValidate")));
     }
 }
