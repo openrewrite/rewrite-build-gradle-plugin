9b0dd9c46fba532508282c8a4f3f79bcf4adb34f
Laurens Westerlaken <laurens.westerlaken@moderne.io>
2026-01-29 15:41:23 +0100

    Pin AssertJ version (#152)

--- src/main/java/org/openrewrite/gradle/RewriteJavaPlugin.java
+++ src/main/java/org/openrewrite/gradle/RewriteJavaPlugin.java
@@ -96,7 +96,7 @@ public class RewriteJavaPlugin implements Plugin<Project> {
         deps.add("testRuntimeOnly", "org.junit.jupiter:junit-jupiter-engine");
         deps.add("testRuntimeOnly", "org.junit.platform:junit-platform-launcher");
 
-        deps.add("testImplementation", "org.assertj:assertj-core:latest.release");
+        deps.add("testImplementation", "org.assertj:assertj-core:3.27.7");
     }
 
     private static void configureJavaCompile(Project project) {
