17596d37bf8b84720d53fb63813fe551ced51703
Shannon Pamperl <shannon@moderne.io>
2026-03-23 18:49:34 -0500

    Upgrade to Jackson 2.21.2

--- src/main/java/org/openrewrite/gradle/RewriteJavaPlugin.java
+++ src/main/java/org/openrewrite/gradle/RewriteJavaPlugin.java
@@ -43,7 +43,7 @@ public class RewriteJavaPlugin implements Plugin<Project> {
         project.getPlugins().apply(RewriteDependencyCheckPlugin.class);
 
         RewriteJavaExtension ext = project.getExtensions().create("rewriteJava", RewriteJavaExtension.class);
-        ext.getJacksonVersion().convention("2.17.2");
+        ext.getJacksonVersion().convention("2.21.2");
 
         project.getPlugins().apply(JavaLibraryPlugin.class);
 
