3109dacbcc90d9514668672afaba0d81460b38dd
Tim te Beek <tim@moderne.io>
2026-08-10 23:19:44 +0200

    Resolve dependencies from CGP before Sonatype snapshots and Maven Central (#206)

--- src/test/java/org/openrewrite/gradle/RewriteDependencyRepositoriesPluginTest.java
+++ src/test/java/org/openrewrite/gradle/RewriteDependencyRepositoriesPluginTest.java
@@ -60,6 +60,24 @@ class RewriteDependencyRepositoriesPluginTest {
         assertThat(result.getOutput()).contains("repo:codegenome=https://artifacts.codegenomeproject.org/maven");
     }
 
+    @Test
+    void codegenomeResolvedBeforeSnapshotsAndMavenCentral(@TempDir File projectDir) throws IOException {
+        writeProject(projectDir);
+
+        BuildResult result = GradleRunner.create()
+                .withProjectDir(projectDir)
+                .withPluginClasspath()
+                .withArguments("printRepositories",
+                        "-PcodegenomeUsername=test-user",
+                        "-PcodegenomePassword=cgp_test-token")
+                .build();
+
+        String output = result.getOutput();
+        assertThat(output.indexOf("repo:codegenome="))
+                .isLessThan(output.indexOf("repo:maven=https://central.sonatype.com/repository/maven-snapshots/"))
+                .isLessThan(output.indexOf("repo:MavenRepo="));
+    }
+
     private static void writeProject(File projectDir) throws IOException {
         writeFile(new File(projectDir, "settings.gradle"), "rootProject.name = 'cgp-consumer'");
         //language=groovy
