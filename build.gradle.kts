9e1af76a8db4908a488d847823998e6c321c8b19
Kevın Carpenter™️ <kwcarpenter@gmail.com>
2026-09-02 15:59:49 -0300

    Bump spring-core to 7.0.9 to clear six Spring web advisories (#225)
    * Bump spring-core to 7.0.9 to clear six Spring web advisories
    
    * Apply suggestion from @kmccarp

--- build.gradle.kts
+++ build.gradle.kts
@@ -263,8 +263,8 @@ dependencies {
         exclude(group = "org.springframework", module = "spring-core")
         exclude(group = "org.springframework", module = "spring-asm")
     }
-    // Provide a newer Spring version to replace the excluded 3.1.3
-    implementation("org.springframework:spring-core:6.2.19")
+    // Spring is only required for PropertyPlaceholderHelper in license-maven-plugin
+    implementation("org.springframework:spring-core:7.0.9")
     implementation("com.github.jk1:gradle-license-report:1.16")
     implementation("org.owasp:dependency-check-gradle:latest.release") {
         exclude(group = "org.apache.lucene", module = "lucene-facet") // 9.12.3 Brings in a vulnerability
